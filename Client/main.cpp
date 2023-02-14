d44c4c9b32cb959ed238634b42c96eb55a3c8348
DisasterLord <101551143+DisasterLord@users.noreply.github.com>
2023-02-14 12:30:38 +0100

    Client Ansichten erstellt
    Die Ansichten:
    - WaitingInQueue
    - Multiplayer
    erstellt und fertig designt :3

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,11 +1,11 @@
 #include "Views/view_menuwindow.h"
-
+#include "Views/view_multiplayer.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    view_MenuWindow w;
+    view_multiplayer w;
     w.show();
     return a.exec();
 }
