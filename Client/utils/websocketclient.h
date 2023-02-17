7d95b423307fb09f194186ef6b9ed8333c042b8d
DisasterLord <101551143+DisasterLord@users.noreply.github.com>
2023-02-17 10:01:48 +0100

    Server verbindung programmiert
    Angefangen die verbindung zum Server und Client zu programmieren

---
+++ Client/utils/websocketclient.h
@@ -0,0 +1,47 @@
+#ifndef WEBSOCKETCLIENT_H
+#define WEBSOCKETCLIENT_H
+
+
+#include <QWidget>
+#include <QAbstractSocket>
+#include <QtWebSockets/QWebSocket>
+
+//#include "../gameManager/playercommunicationmanager.h"
+
+
+class WebSocketClient : public QObject
+{
+    Q_OBJECT
+
+public:
+    static WebSocketClient& getInstance()
+    {
+        static WebSocketClient instance;
+        return instance;
+    }
+
+    void connectToServer(const QUrl &url);
+    void sendMessage(const QString &message);
+
+signals:
+    void connected();
+    void disconnected();
+    void messageReceived(const QString &message);
+    void error(const QString &errorString);
+
+private slots:
+    void onConnected();
+    void onDisconnected();
+    void onError(QAbstractSocket::SocketError error);
+
+private:
+    WebSocketClient(QObject *parent = nullptr);
+    ~WebSocketClient();
+    Q_DISABLE_COPY(WebSocketClient)
+
+    QWebSocket *m_webSocket;
+  /*  playerCommunicationManager *pmm;
+    */
+};
+
+#endif // WEBSOCKETCLIENT_H
